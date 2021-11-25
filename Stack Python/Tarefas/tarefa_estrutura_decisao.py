888a3b3ff3a2c2210d0d7b381d0d144e32371be5
pedro-toodoo <pedro.abritta@toodoo.com.br>
2021-11-25 16:42:43 -0300

    Add tarefa de estrutura de dados

---
+++ Stack Python/Tarefas/tarefa_estrutura_decisao.py
@@ -0,0 +1,24 @@
+num = float(input("Digite um número: "))
+
+print("Digite 1 para saber se o número é par ou ímpar")
+print("Digite 2 para saber se o número é positivo ou negativo")
+print("Digite 3 para saber se o número é inteiro ou decimal")
+opcao = int(input("Opção: "))
+
+if opcao == 1:
+    if num % 2 == 0:
+        print(f"{num} é par!")
+    else:
+        print(f"{num} é ímpar!")
+
+elif opcao == 2:
+    if num >= 0:
+        print(f"{num} é positivo!")
+    else:
+        print(f"{num} é negativo!")
+
+elif opcao == 3:
+    if type(num) == int:
+        print(f"{num} é inteiro!")
+    elif type(num) == float:
+        print(f"{num} é decimal!")
