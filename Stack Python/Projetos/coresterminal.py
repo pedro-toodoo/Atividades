15a675b3c213311660505305187ac32243e2cfc2
pedro-toodoo <pedro.abritta@toodoo.com.br>
2021-11-26 13:59:04 -0300

    atualizado

---
+++ Stack Python/Projetos/coresterminal.py
@@ -0,0 +1,29 @@
+#\033[STYLE;TEXT;BACKm]
+
+#stye:
+#0 - none
+#1 - bold
+#4 - underline
+#7 - negative
+#--------------
+#text color:
+#30 - branco
+#31 - vermelho
+#32 - verde
+#33 - amarelo
+#34 - azul
+#35 - magenta
+#36 - ciano
+#37 - cinza
+#--------------
+#background:
+#40 - branco
+#41 - vermelho
+#42 - verde
+#43 - amarelo
+#44 - azul
+#45 - magenta
+#46 - ciano
+#47 - cinza
+
+print("\033[35;40mHello World!\033[m")
---
+++ Stack Python/Projetos/estrutura_repeticao.py
@@ -0,0 +1,4 @@
+for i in range(0, 3):
+    print(f"{i} Fala cmg bb")
+for i in range(2, -1, -1):
+    print(f"{i} Fala cmg bb")
