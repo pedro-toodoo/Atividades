754d8e72c3e6081f2d841efb0e4b113c3ad2cff6
pedro-toodoo <pedro.abritta@toodoo.com.br>
2021-11-26 15:58:58 -0300

    add estrutura repeticao while

---
+++ Stack Python/Projetos/estrutura_repeticao_while.py
@@ -0,0 +1,8 @@
+i = 0
+while i < 4:
+    print(i)
+    i += 1
+
+num = int(input("Informe um número (0 para sair): "))
+while num != 0: #FLAG
+    num = int(input("Informe um número (0 para sair): "))
