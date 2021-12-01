ffaf4660bd7b8ba141a8e3f7fb5fc5a192842a78
pedro-toodoo <pedro.abritta@toodoo.com.br>
2021-12-01 12:56:06 -0300

    Adicionado tarefa1_funcoes

---
+++ Stack Python/Tarefas/tarefa1_funcoes.py
@@ -0,0 +1,10 @@
+
+def imprime(n):
+    for i in range(0, n):
+        for j in range(0, i+1):
+            print(f"{j+1}", end=" ")
+        print()
+
+num = int(input("Digite um número: "))
+imprime(num)
+
