81f36ea94240e44bb498f4ceea1486c934fe903f
pedro-toodoo <pedro.abritta@toodoo.com.br>
2021-12-22 17:07:36 -0300

    Atualizado main mongoDB

--- Stack Python/Projetos/Banco de Dados/MongoDB/main.py
+++ Stack Python/Projetos/Banco de Dados/MongoDB/main.py
@@ -86,22 +86,111 @@ if __name__ == '__main__':
     #BUSCAS
     leitos = db.leitos
     pacientes = db.pacientes
-    result = pacientes.find()
 
+    '''filter: {leito: {$gte: 34}}
+     $eq = 
+     $lt <
+     $gte >='''
+
+    '''
+    Project: {nome: 1, leito: 1, _id:0} _id:0 desabilita chave primária
+    '''
+    '''
+    sort: {nome: 1} 1 crescente, -1 decrescente
+    '''
+
+    """result = pacientes.find({'leito': {'$gte': 35}})
     for i in result:
         print(i)
+    print("-"*30)
 
+    result = leitos.find()
+    for i in result:
+        print(i)"""
 
+    #ATUALIZAR
+    '''result = pacientes.find()
+    for i in result:
+        print(i['nome'])
+    print("-"*30)
 
+    result = pacientes.update_one(
+        {'cpf': '111.222.333-45'},
+        {'$set': {'nome': 'Homem aranha'}}
+    )
+    verifica(result)
 
+    result = pacientes.find()
+    for i in result:
+        print(i['nome'])
+    print("-"*30)'''
 
+    #EXCLUIR
+    '''result = pacientes.find()
+    for i in result:
+        print(i['nome'])
+    result = pacientes.delete_one({'leito': 32})
+    verifica(result)
 
+    print("-" * 30)
 
+    result = pacientes.find()
+    for i in result:
+        print(i['nome'])
+    print("-" * 30)'''
 
+    '''result = leitos.find()
+    for i in result:
+        print(i['numero'])
 
+    result = leitos.delete_one({'numero': 32})
+    verifica(result)
+    print("-"*20)
+    result = leitos.find()
+    for i in result:
+        print(i['numero'])'''
 
+    #AGREGAÇÃO
+    ag1 = {
+        '$lookup': {
+            'from': 'leitos', #coleção estrangeira
+            'localField': 'leito', #coleção local
+            'foreignField': 'numero', #campo estrangeiro
+            'as': 'LEITO'
+        }
+    }
 
+    ag2 = {
+        '$project': {
+            '_id': 0,
+            'Nome': '$nome',
+            'Ano': '$ano_nasc',
+            'Dor': '$enfermidade',
+            'Leito': '$leito',
+            'Info': {'$arrayElemAt': ['$LEITO', 0]}
+        }
+    }
 
+    ag3 = {
+        '$project': {
+            'Nome': '$Nome',
+            'Ano': '$Ano',
+            'Dor': '$Dor',
+            'Leito': '$Leito',
+            'Sala': '$Info.sala',
+            'Analgesico': '$Info.medicamento.analgesico'
+        }
+    }
+
+    result = pacientes.aggregate([ag1, ag2, ag3])
+    for i in result:
+        print('Nome:', i['Nome'])
+        print('Ano de Nascimento:', i['Ano'])
+        print('Enfermidade:', i['Dor'])
+        print('Leito:', i['Leito'])
+        print('Sala:', i['Sala'])
+        print('Analgésico:', i['Analgesico'])
+        print("-"*30)
 
 
 
